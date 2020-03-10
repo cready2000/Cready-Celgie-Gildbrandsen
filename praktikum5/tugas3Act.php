46a3e0da1d7d298e50e5009f8d1744ebd50d0fb5
cready2000 <61173244+cready2000@users.noreply.github.com>
2020-03-10 18:47:14 +0700

    praktikum 5 pemrograman web
    Cready Celgie Gildbrandsen (18082010031)

---
+++ praktikum5/tugas3Act.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+	<?php
+	if (empty($_POST['namalengkap'])) {
+		header("Location:tugas3kosong.php");
+	} else if (empty($_POST['namapanggilan'])) {
+		header("Location:tugas3kosong.php");
+	} else if (empty($_POST['npm'])) {
+		header("Location:tugas3kosong.php");
+	} else if (empty($_POST['ttl'])) {
+		header("Location:tugas3kosong.php");
+	} else if (empty($_POST['pekerjaan'])) {
+		header("Location:tugas3kosong.php");
+	} else if (empty($_POST['hobby'])) {
+		header("Location:tugas3kosong.php");
+	} else if (empty($_POST['citacita'])) {
+		header("Location:tugas3kosong.php");
+	} else if (empty($_POST['nohp'])) {
+		header("Location:tugas3kosong.php");
+	} else if (empty($_POST['idline'])) {
+		header("Location:tugas3kosong.php");
+	} else if (empty($_POST['email'])) {
+		header("Location:tugas3kosong.php");
+	} else {
+		echo "<center> Nama Lengkap :".$_POST['namalengkap']."</center><br>";
+		echo "<center> Nama Panggilan :".$_POST['namapanggilan']."</center><br>";
+		echo "<center> NPM :".$_POST['npm']."</center><br>";
+		echo "<center> Tempat, Tanggal Lahir :".$_POST['ttl']."</center><br>";
+		echo "<center> Pekerjaan :".$_POST['pekerjaan']."</center><br>";
+		echo "<center> Hobby :".$_POST['hobby']."</center><br>";
+		echo "<center> Cita-Cita :".$_POST['citacita']."</center><br>";
+		echo "<center> Nomor HP :".$_POST['nohp']."</center><br>";
+		echo "<center> ID Line :".$_POST['idline']."</center><br>";
+		echo "<center> Email :".$_POST['email']."</center><br>";
+		date_default_timezone_set("Asia/Jakarta");
+		echo "<center> Login pada ".date("D, d F Y G:i:s"."</center>");
+	}
+	?>
+</body>
+</html>
