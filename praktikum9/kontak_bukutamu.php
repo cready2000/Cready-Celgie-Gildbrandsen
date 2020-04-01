e0ee5c7378b7c18af1c18c336f8a7ed30d3dc137
cready2000 <61173244+cready2000@users.noreply.github.com>
2020-04-01 09:29:15 +0700

    praktikum 9 pemrograman web
    Cready Celgie Gildbrandsen (18082010031)

---
+++ praktikum9/kontak_bukutamu.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> BUKU TAMU </title>
+</head>
+<body>
+	<center>
+	<h1> BUKU TAMU </h1>
+		<form action="" method="POST">
+			<table>
+				<tr>
+					<td> Nama </td>
+					<td> : 
+						<input type="text" name="nama" required/> </td>
+				</tr>
+				<tr>
+					<td> Jenis Kelamin </td>
+					<td> :
+						<input type="text" name="jkel" required/> </td>
+				</tr>
+				<tr>
+					<td> Email </td>
+					<td> :
+						<input type="text" name="email" required/> </td>
+				</tr>
+				<tr>
+					<td> Alamat </td>
+					<td> :
+						<input type="text" name="alamat" required/> </td>
+				</tr>
+				<tr>
+					<td> Kota </td>
+					<td> :
+						<input type="text" name="kota" required/> </td>
+				</tr>
+				<tr>
+					<td> Pesan </td>
+					<td> :
+						<input type="text" name="pesan" required/> </td>
+				</tr>
+				<tr>
+					<td> <input type="submit" name="btnSimpan" value="Simpan"> </td>
+					<td> <input type="reset" name="reset" value="Reset"></td>
+				</tr>
+			</table>
+		</form>
+	</center>
+	
+</body>
+</html>
