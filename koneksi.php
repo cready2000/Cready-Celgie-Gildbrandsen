38ca558e6e7c727f8c08c3bef2a95c6e547d3d6e
cready2000 <61173244+cready2000@users.noreply.github.com>
2020-04-13 20:10:00 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "myDB";
-$conn=mysqli_connect($servername, $username, $password, $dbname) or die (mysqli_error());
-if (!$conn) {
-	die("Connection failed ".mysqli_connect_error());
-	}
